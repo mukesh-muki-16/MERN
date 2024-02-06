6e7e613a9fbdeb49cbf2494ebb0436dd8d4a2e9e
Mukesh <mukeshchevula@gmail.com>
2024-02-06 21:17:01 +0530

    AUTH MIDDLEWARE DONE

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,22 @@
 import bcrypt from 'bcryptjs'
 
 const users = [
-    {
-        name: 'Admin ',
-        email: "admin@ind.com",
-        password :  bcrypt.hashSync('123456', 10),
-        isAdmin: true
-    },    
-    {
-        name: 'user 1 ',
-        email: "user1@ind.com",
-        password :bcrypt.hashSync('123456', 10),
-    },    
-    {
-        name: 'user 2 ',
-        email: "user2@ind.com",
-        password :bcrypt.hashSync('123456', 10),
-    },
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
 ]
 
 export default users
