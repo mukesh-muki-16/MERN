465a7e9280b0e89ea7d5592d822df82327e54b1d
Mukesh <mukeshchevula@gmail.com>
2024-02-08 18:24:56 +0530

    Order Screen Improved

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,26 +1,34 @@
 import asyncHandler from 'express-async-handler'
-import OrderModel from '../models/orderModel.js'
 import Order from '../models/orderModel.js'
 
-// @desc    create new order
+// @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  const{orderItems,shippingAddress,paymentMethod,itemsPrice,taxPrice,shippingPrice,totalPrice} = req.body
-  if(orderItems&& orderItems.length===0){
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body
+
+  if (orderItems && orderItems.length === 0) {
     res.status(400)
-    throw new Error('Please add atleast one item')
+    throw new Error('No order items')
     return
-  }else{
+  } else {
     const order = new Order({
-        orderItems,
-        user: req.user._id,
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
     })
 
     const createdOrder = await order.save()
@@ -29,6 +37,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-export {
-    addOrderItems,
-}
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById }
