8e465534f63fcf3a9c65c35c6fd509659d3d78e6
Mukesh <mukeshchevula@gmail.com>
2024-02-09 16:48:47 +0530

    product edit by admin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,22 @@
-import express from 'express'
-const router = express.Router()
+import express from 'express';
+const router = express.Router();
 import {
   getProducts,
   getProductById,
-  deleteProduct
-} from '../controllers/productController.js'
-import { protect, admin } from '../middleware/authMiddleware.js'
+  deleteProduct,
+  createProduct,
+  updateProduct,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
+// Routes for fetching all products and creating a new product
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+// Routes for fetching, updating, and deleting a single product by ID
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
-
-export default router
+export default router;
